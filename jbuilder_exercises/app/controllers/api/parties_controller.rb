e5de4ef96e598224832a61509a41f82e2ff3593d
App Academy Student <jtdespinosa@gmail.com>
2019-01-16 17:58:11 -0500

    Finished jbuilder bonus phase

--- jbuilder_exercises/app/controllers/api/parties_controller.rb
+++ jbuilder_exercises/app/controllers/api/parties_controller.rb
@@ -1,11 +1,11 @@
 class Api::PartiesController < ApplicationController
   def index
-    @parties = Party.all
+    @parties = Party.all.includes(guests: [:gifts])
     render :index
   end
 
   def show
-    @party = Party.find_by(id: params[:id])
+    @party = Party.includes(guests: [:gifts]).find_by(id: params[:id])
     render :show
   end
 end
--- jbuilder_exercises/app/views/api/guests/index.json.jbuilder
+++ jbuilder_exercises/app/views/api/guests/index.json.jbuilder
@@ -1,4 +1,8 @@
-json.array! @guests do |guest|
+middleAgedGuests = @guests.select do |guest|
+  guest.age.between?(40, 50);
+end
+
+json.array! middleAgedGuests do |guest|
   json.partial! 'api/guests/guest', guest: guest
 end
 
